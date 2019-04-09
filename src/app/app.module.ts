ee1f4a6583f9548a745d5a027151da4a2f8dea27
hichem rtibi <hichem.rtibi@gmail.com>
2019-04-10 00:53:40 +0200

    create component et installation de bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import { UserSettingsFromComponent } from './user-settings-from/user-settings-from.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserSettingsFromComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/user-settings-from/user-settings-from.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-settings-from',
+  templateUrl: './user-settings-from.component.html',
+  styleUrls: ['./user-settings-from.component.css']
+})
+export class UserSettingsFromComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
