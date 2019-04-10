5abc7f6c69d46fa081e51bb76747fe5f37cda329
hichem rtibi <hichem.rtibi@gmail.com>
2019-04-10 02:19:31 +0200

    creationmodel and binding two way

---
+++ src/app/data/user.settings.ts
@@ -0,0 +1,8 @@
+
+export interface  usersettings {
+  name:string;
+  emailOffers:boolean;
+  interfacestyle:string;
+  subscriptionType:string;
+  notes:string;
+}
--- src/app/user-settings-from/user-settings-from.component.ts
+++ src/app/user-settings-from/user-settings-from.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {usersettings} from '../data/user.settings';
 
 @Component({
   selector: 'app-user-settings-from',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-settings-from.component.css']
 })
 export class UserSettingsFromComponent implements OnInit {
+  usersettings:usersettings=
+    {
+      name:'hichem',
+      emailOffers:true,
+      interfacestyle:'Dark',
+      subscriptionType:'Annualy',
+      notes:'he are some ........'
+
+    }
 
   constructor() { }
 
